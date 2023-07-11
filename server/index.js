f1d1884fa89248491aef30612f2213dcc1e46346
manishh-sharma <135424636+manishh-sharma@users.noreply.github.com>
2023-07-11 22:22:15 +0530

    final commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const routes = require('./routes');
+const handle = require('./handlers');
+const path = require('path');
+const { expr } = require('jquery');
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname,"./public")));
+
+
+
+app.use('/api/auth', routes.auth);
+app.use('/api/polls', routes.poll);
+
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+app.use(handle.error);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
